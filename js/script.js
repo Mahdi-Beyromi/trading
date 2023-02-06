5b7aa17f18990c5835054648b461c282fc9f1f45
Mahdi Beiromi <100340030+mombey10382@users.noreply.github.com>
2023-02-07 00:33:43 +0330

    change to trading web

--- js/script.js
+++ js/script.js
@@ -53,40 +53,42 @@
         }
     });
     
-        // Back to top button
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 300) {
-                $('.back-to-top').fadeIn('slow');
-            } else {
-                $('.back-to-top').fadeOut('slow');
-            }
-        });
-        $('.back-to-top').click(function () {
-            $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-            return false;
-        });
     
-    
-        // Testimonials carousel
-        $(".testimonial-carousel").owlCarousel({
-            autoplay: true,
-            smartSpeed: 1000,
-            center: true,
-            margin: 24,
-            dots: true,
-            loop: true,
-            nav : false,
-            responsive: {
-                0:{
-                    items:1
-                },
-                768:{
-                    items:2
-                },
-                992:{
-                    items:3
-                }
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 24,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
             }
-        });
-        
-    })(jQuery);
+        }
+    });
+    
+})(jQuery);
+
